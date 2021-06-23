31b41795934839b5507255d3ef7ff8feb6d72771
Hemant <srihemant@outlook.com>
2021-06-23 10:37:24 +0530

    Styling & Theming

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -5,11 +5,15 @@ class CategoryItem extends StatelessWidget {
   final Color color;
 
   CategoryItem(this.title, this.color);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(15),
-      child: Text(title),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.title,
+      ),
       decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import './categories.dart';
+
+import './categories_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,9 +8,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'WeEat',
+      title: 'WeServe',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.lightGreen,
+        accentColor: Colors.amberAccent,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+            body1: TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            body2: TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            title: TextStyle(
+              fontSize: 20,
+              fontFamily: 'RobotoCondensed',
+              fontWeight: FontWeight.bold,
+            )),
       ),
       home: CategoriesScreen(),
     );
