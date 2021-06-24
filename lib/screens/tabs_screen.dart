1d3f9cf02fd120200a6bd6675f8cb140ca7f360a
Hemant <srihemant@outlook.com>
2021-06-24 22:28:37 +0530

    adding TabBar to AppBar

---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'favorites_screen.dart';
+import './categories_screen.dart';
+
+class TabScreen extends StatefulWidget {
+  @override
+  _TabScreenState createState() => _TabScreenState();
+}
+
+class _TabScreenState extends State<TabScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('WeServe'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(
+                  Icons.category,
+                ),
+                text: 'Categories',
+              ),
+              Tab(
+                icon: Icon(
+                  Icons.star,
+                ),
+                text: 'Favorites',
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            CategoriesScreen(),
+            FavoriteScreen(),
+          ],
+        ),
+      ),
+    );
+  }
+}
