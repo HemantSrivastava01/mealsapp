5594c7aae80f4775cc02aee879e791877e1c04c4
Hemant <srihemant@outlook.com>
2021-06-24 09:36:05 +0530

    navigating to meal detail

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './screens/meal_details_screen.dart';
 import './screens/category_meals_screen.dart';
 
 import './screens/categories_screen.dart';
@@ -32,7 +33,8 @@ class MyApp extends StatelessWidget {
       initialRoute: '/', //default is '/'
       routes: {
         '/': (ctx) => CategoriesScreen(),
-        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen()
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
     );
   }
